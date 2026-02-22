754af72dd204f9736dd4fde8523af5e7f34b4b8d
Amirhoseinbabaie <amirhosein.babaie.moghadam@gmail.com>
2026-02-22 15:52:18 +0330

    Add Post model, migration, seeder And factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Post::class;
+
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'excerpt' => fake()->paragraph(),
+            'body' => fake()->paragraph(),
+            'author' => fake()->name(),
+            'views' => fake()->randomDigit(),
+            'published' => fake()->boolean()
+        ];
+    }
+}
